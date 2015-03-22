3e210a4cf27c67e2dd3b2547c9b158d5dd4f174e
shusijie1992 <shusijie1992@126.com>
2015-03-23 01:32:56 +0800

    Create plot6.R

---
+++ plot6.R
@@ -0,0 +1,22 @@
+#Read Data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+#Merge data
+merged <- merge(NEI, SCC[,c(1,4)], by.x = "SCC", by.y = "SCC", all.x = TRUE)
+merged$EI.Sector <- as.character(merged$EI.Sector)
+#Filter
+filtered <- merged[grep("Vehicle", merged$EI.Sector),]
+
+#Calculate sum of emissions per year in Baltimore & Los Angeles County
+agg <- aggregate(Emissions ~ year * fips, data = filtered[filtered$fips == "24510" | filtered$fips == "06037",], FUN = sum)
+agg$fips[agg$fips == "24510"] <- "Baltimore"
+agg$fips[agg$fips == "06037"] <- "Los Angeles"
+
+#Plot the result of sum of emissions on the basis of years
+library(ggplot2)
+qplot(data = agg, x = agg$year, y = agg$Emissions, facets=.~fips, geom = c("line"), main = "PM2.5 Emissions in Baltimore & Los Angeles County from motor vehicle sources", xlab = "Year", ylab = "Emissions")
+
+#Save the plot as .png file
+dev.copy(device = png, file = "plot6.png", width = 480, height = 480)
+dev.off()
