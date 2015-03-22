f1a0100693c5d05a593c4f4c6eb5242841beb43d
shusijie1992 <shusijie1992@126.com>
2015-03-23 01:32:09 +0800

    Create plot5.R

---
+++ plot5.R
@@ -0,0 +1,20 @@
+#Read Data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+#Merge data
+merged <- merge(NEI, SCC[,c(1,4)], by.x = "SCC", by.y = "SCC", all.x = TRUE)
+merged$EI.Sector <- as.character(merged$EI.Sector)
+#Filter
+filtered <- merged[grep("Vehicle", merged$EI.Sector),]
+
+#Calculate sum of emissions per year in Baltimore
+agg <- aggregate(Emissions ~ year, data = filtered[filtered$fips == "24510",], FUN = sum)
+
+#Plot the result of sum of emissions on the basis of years
+library(ggplot2)
+qplot(data = agg, x = agg$year, y = agg$Emissions, geom = c("line"), main = "PM2.5 Emissions in Baltimore from motor vehicle sources", xlab = "Year", ylab = "Emissions")
+
+#Save the plot as .png file
+dev.copy(device = png, file = "plot5.png", width = 480, height = 480)
+dev.off()
